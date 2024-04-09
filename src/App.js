eb748121e1926d052d953af87c573df00410fe07
Elijah Anderson <eja11001@DESKTOP-83DSK5F.localdomain>
2024-04-09 09:33:49 -0700

    Initial commit of src

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from "./Views/HomePage/HomePage";
-import Forecasts from './Views/Forecasts/Forecasts';
-import TabWrapper from "./Components/Tabs/TabWrapper";
 import Header from './Components/Headers/AppHeader';
 import Login from './Components/Auth/Login';
 import ProtectedRoute from './Components/Auth/ProtectedRoute';
@@ -24,18 +22,6 @@ function App() {
                     <ProtectedRoute>
                         <Header />
                         <HomePage />
-                    </ProtectedRoute>
-                } />
-                <Route path="/forecasts/:etfName" element={
-                    <ProtectedRoute>
-                        <Header />
-                        <Forecasts />
-                    </ProtectedRoute>
-                } />
-                <Route path="/model_metadata/:etfName/:model_id" element={
-                    <ProtectedRoute>
-                        <Header />
-                        <TabWrapper />
                     </ProtectedRoute>
                 } />
             </Routes>
